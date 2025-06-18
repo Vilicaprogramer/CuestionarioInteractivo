a0fd383bf493cd8a260ffba687ed7a2b1b72716d
Vicen <150998620+Vilicaprogramer@users.noreply.github.com>
2025-06-18 16:48:00 +0200

    Mejoras en el menu

--- funciones.py
+++ funciones.py
@@ -9,28 +9,83 @@ def preguntar_nombre():
     return input("Cual es tu nombre? ")
 
 def menu_principal():
-    print('''### MENÚ CUESTIONARIO INTERACTIVO ###
+    print ('''### MENÚ CUESTIONARIO INTERACTIVO ###
 1 - Empezar cuestionario
 2 - Ranking (opcional)
-3 - Salir''')
+3 - Salir\n''')
+    
+def menu_cuestionarios():
+    print('''### ELIGE UNA TEMÁTICA ###
+1 - Programación
+2 - SQL y BBDD
+3 - HTML, CSS, JavaScript
+4 - Cultura general
+5 - Salir\n''')
 
-def empieza_cuestionario():
-    with open("preguntas.json", "r", encoding="utf-8") as archivo:
+
+def continuar_preguntando():
+    while True:
+        try:
+            continuar = int(input('''¿QUIERES CONTINUAR?
+                                1 - Para continuar    2 - Para salir:\t   '''))
+            if continuar == 1:
+                return True
+            else:
+                return False
+        except:
+            print("Opción incorrecta, intentalo de nuevo: ")
+    
+        
+def empieza_cuestionario(cuestionario):
+    with open(cuestionario, "r", encoding="utf-8") as archivo:
         preguntas = json.load(archivo)
     
-    num_aleatorio = randint(1,20)
+    puntuacion = 0
+    cambio_de_tema = True
+    while cambio_de_tema:
+        try:
+            preguntas_hechas = []
+            num_aleatorio = randint(1,20)
+            if num_aleatorio not in preguntas_hechas:   
+                for pregunta in preguntas["preguntas"]:
+                    if pregunta["numero"] == num_aleatorio:
+                        print(pregunta["pregunta"])
+                        for opcion in pregunta["opciones"]:
+                            print(opcion)
 
-    for pregunta in preguntas["preguntas"]:
-        if pregunta["numero"] == num_aleatorio:
-            print(pregunta["pregunta"])
-            for opcion in pregunta["opciones"]:
-                print(opcion)
-            print(f"Respuesta correcta: {pregunta['respuesta_correcta']}")
-            print()
+                        print()
+                        repuesta = input("¿Cual es tu respuesta?: ").upper()
+                        print()
+                        if repuesta == pregunta['respuesta_correcta']:
+                            print('¡¡¡CORRECTO!!!')
+                            puntuacion += 10
+                            continuar = int(input("1 para continuar 2 para salir: \t"))
+                            print()
+                            if continuar == True:
+                                continue
+                            else:
+                                cambio_de_tema = False
+                        else:
+                            print(f"Respuesta correcta: {pregunta['respuesta_correcta']}")
+                            print()
+                else:
+                    continue
+        except:
+            print("Opción incorrecta, intentalo de nuevo: ")
+       
+    return puntuacion
 
 
-def guardar_ranking():
-    pass
+def guardar_ranking(nombre, puntuacion):
+    with open("ranking.json", "r", encoding="utf-8") as f:
+        datos = json.load(f)
+    nueva_puntuacion = {"nombre": nombre, "puntuacion": puntuacion}
+
+    datos["ranking"].append(nueva_puntuacion)
+
+    with open("ranking.json", "w", encoding="utf-8") as f:
+        json.dump(datos, f, ensure_ascii=False, indent=4)
+
 
 def mostrar_ranking():
     pass
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import funciones as f
 
+f.bienvenida()
 nombre = f.preguntar_nombre()
 suma_puntos = 0
 ranking_jugador = {}
 ranking_jugador["nombre"] = nombre
 
+puntuacion = 0
 opcion = 0
 while opcion != 3:
     f.menu_principal()
     try:
         opcion = int(input("Introduce una opción: "))
         if opcion == 1:
-            f.empieza_cuestionario()
+            eleccion = 0
+            while eleccion != 5:
+                f.menu_cuestionarios()
+                try:
+                    eleccion = int(input("¿Qué cuestionario quieres?: "))
+                    if eleccion == 1:
+                        puntuacion += f.empieza_cuestionario("preguntas_programacion.json")
+                    elif eleccion == 2:
+                        puntuacion += f.empieza_cuestionario("preguntas_sql_bases_datos.json")
+                    elif eleccion == 3:
+                        puntuacion += f.empieza_cuestionario("preguntas_html_css_js.json")
+                    elif eleccion == 4:
+                        puntuacion += f.empieza_cuestionario("preguntas_cultura_general.json")
+                except:
+                    print("No es una opción válida, intentalo de nuevo")  
+            print(f'\nTu puntuación ha sido de {puntuacion} puntos \n') 
+            f.guardar_ranking(nombre, puntuacion)   
         elif opcion == 2:
-            f.mostrar_ranking()
-        else:
-            print("No es una opción válida, intentalo de nuevo")
+            pass
     except:
         print("No es una opción válida, intentalo de nuevo")
